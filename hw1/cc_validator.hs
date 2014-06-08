d62e4bf07e438359d1a48a2cc4c0f44e86124cd9
Rahul Horé <hore.rahul@gmail.com>
2014-06-07 21:17:52 -0400

    Adds (ugly but functional) doubleEveryOther method

--- hw1/cc_validator.hs
+++ hw1/cc_validator.hs
@@ -11,3 +11,14 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev cc
   | cc <= 0   = []
   | otherwise = cc `mod` 10 : toDigitsRev (cc `div` 10)
+
+-- Exercise 2
+
+-- Double every second digit from the left
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft (x:y:xs) = x : 2 * y : doubleEveryOtherFromLeft xs
+doubleEveryOtherFromLeft a = a
+
+-- Double every second digit from the right
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther x = reverse (doubleEveryOtherFromLeft (reverse x))
