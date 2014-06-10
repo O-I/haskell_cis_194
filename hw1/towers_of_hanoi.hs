cf0b502a4f1af451cf3e337976fce393296f7fb9
Rahul Horé <hore.rahul@gmail.com>
2014-06-10 15:25:12 -0400

    Solves Reve's puzzle, but not optimal solution

--- hw1/towers_of_hanoi.hs
+++ hw1/towers_of_hanoi.hs
@@ -8,3 +8,12 @@ hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n s d u
   | n <= 0 = []
   | n >  0 = hanoi (n-1) s u d ++ [(s, d)] ++ hanoi (n-1) u d s
+
+-- Four-peg Towers of Hanoi (not optimal)
+-- Moves a stack of discs from first peg to last
+-- where s = source, x = using1, y = using2, d = destination
+hanoi4Peg :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4Peg n s x y d
+  | n <= 0 = []
+  | n == 1 = [(s, d)]
+  | n >  1 = hanoi4Peg (n-2) s y d x ++ [(s, y), (s, d), (y, d)] ++ hanoi4Peg (n-2) x s y d
