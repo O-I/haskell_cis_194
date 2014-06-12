da37eab4801d27e658bcd479c5d4b0d1ccc66a76
Rahul Horé <hore.rahul@gmail.com>
2014-06-12 16:55:40 -0400

    Adds unrefined parseMessage function

---
+++ hw2/LogAnalysis.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+
+import Log
+
+-- Exercise 1
+
+-- Parses an individual log message
+
+parseMessage :: String -> LogMessage
+parseMessage message
+  | take 2 message == "I " = LogMessage Info
+                                        (read (words message !! 1) :: Int)
+                                        (unwords (drop 2 (words message)))
+  | take 2 message == "W " = LogMessage Warning
+                                        (read (words message !! 1) :: Int)
+                                        (unwords (drop 2 (words message)))
+  | take 2 message == "E " = LogMessage (Error
+                                        (read (words message !! 1) :: Int))
+                                        (read (words message !! 2) :: Int)
+                                        (unwords (drop 3 (words message)))
+  | otherwise              = Unknown message
