c84ea0296f80829c36093c6fc0425de658d11798
Rahul Horé <hore.rahul@gmail.com>
2014-06-12 18:49:57 -0400

    Completes parseMessage refactor

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -10,20 +10,14 @@ import Log
 
 parseMessage :: String -> LogMessage
 parseMessage message
-  | kind == "I"  = LogMessage Info
-                   (intArg 1)
-                   (unwords (drop 2 msg))
-  | kind == "W"  = LogMessage Warning
-                   (intArg 1)
-                   (unwords (drop 2 msg))
-  | kind == "E"  = LogMessage (Error
-                   (intArg 1))
-                   (intArg 2)
-                   (unwords (drop 3 msg))
+  | kind == "I"  = LogMessage  Info    (intArg 1)  (strArg 2)
+  | kind == "W"  = LogMessage  Warning (intArg 1)  (strArg 2)
+  | kind == "E"  = LogMessage (Error   (intArg 1)) (intArg 2) (strArg 3)
   | otherwise    = Unknown (unwords msg)
   where msg      = words message
         kind     = head msg
         intArg n = read (msg !! n) :: Int
+        strArg n = unwords (drop n msg)
 
 -- Parses an entire log file
 
