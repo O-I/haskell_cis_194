ea40e4ed9500355285b74e5c80cccb460a8743dd
Rahul Horé <hore.rahul@gmail.com>
2014-06-15 19:08:48 -0400

    Makes style consistent with other HW solutions

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -6,8 +6,7 @@ import Log
 
 -- Exercise 1
 
--- Parses an individual log message
-
+-- Parse an individual log message
 parseMessage :: String -> LogMessage
 parseMessage message
   | kind == "I"  = LogMessage  Info    (intArg 1)  (strArg 2)
@@ -19,15 +18,13 @@ parseMessage message
         intArg n = read (msg !! n) :: Int
         strArg n = unwords (drop n msg)
 
--- Parses an entire log file
-
+-- Parse an entire log file
 parse :: String -> [LogMessage]
 parse file = map parseMessage (lines file)
 
 -- Exercise 2
 
--- Inserts a LogMessage into a MessageTree
-
+-- Insert a LogMessage into a MessageTree
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree     = tree
 insert message Leaf         = Node Leaf message Leaf
@@ -35,31 +32,27 @@ insert message (Node l m r)
   | message `after` m = Node l m (insert message r)
   | otherwise         = Node (insert message l) m r
 
--- Compares two LogMessages by timestamp
-
+-- Compare two LogMessages by timestamp
 after :: LogMessage -> LogMessage -> Bool
 after (LogMessage _ t1 _) (LogMessage _ t2 _) = t1 > t2
 after _ _ = error "Missing timestamp"
 
 -- Exercise 3
 
--- Builds a MessageTree given a list of LogMessages
-
+-- Build a MessageTree given a list of LogMessages
 build :: [LogMessage] -> MessageTree
 build = foldr insert Leaf
 
 -- Exercise 4
 
--- Produces a list of LogMessages sorted by timestamp
-
+-- Produce a list of LogMessages sorted by timestamp
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf         = []
 inOrder (Node l m r) = inOrder l ++ [m] ++ inOrder r
 
 -- Exercise 5
 
--- Extracts LogMessages with severity >= 50 from unsorted list
-
+-- Extract LogMessages with severity >= 50 from unsorted list
 whatWentWrong :: [LogMessage] -> [String]
 whatWentWrong msgs =
   [msg | m@(LogMessage _ _ msg) <- inOrder (build msgs), severityAtAndOver50 m]
