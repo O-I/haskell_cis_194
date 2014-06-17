6e0c5d76bca7f15205c442f56371b37aa441d4c3
Rahul Horé <hore.rahul@gmail.com>
2014-06-17 11:34:35 -0400

    Gets everyNth for every 1 to length a of list a

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,5 +1,10 @@
 module Golf where
 
+-- Exercise 1  Hopscotch
+
+skips :: [a] -> [[a]]
+skips l = map (\n -> everyNth n l) [1..length l]
+
 everyNth :: Int -> [a] -> [a]
 everyNth _ [] = []
-everyNth n a = map fst [x | x <- zip a [1..length a], (snd x) `mod` n == 0]
+everyNth n a  = map fst [x | x <- zip a [1..length a], (snd x) `mod` n == 0]
