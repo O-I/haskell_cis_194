8bc89df94005c833c6d4ca97e68b431671368ccb
Rahul Horé <hore.rahul@gmail.com>
2014-06-23 18:54:41 -0400

    Refactors fun2
    Homework 4 exercise 1 asks you to refactor using wholemeal programming techniques. You are given a hint to use `takeWhile` and `iterate` to refactor `fun2`. I did this after looking at other solutions to see if there was any way to do this without `if`-`then`-`else`. I think the hangup for me is that although I see the logic here, I don't see this as an improvement over the original `fun2` and I think of refactoring as an exercise in improving code. I really dislike the where clause which just seems like a more awkward way of the original solution using guards. As opposed to the `fun1` refactor, I think the `fun2` refactor fails to make the code better.

--- hw4/wholemeal.hs
+++ hw4/wholemeal.hs
@@ -12,3 +12,16 @@ fun1 (x:xs)
 
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
+
+-- Original fun2
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+-- Refactored fun2
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/= 1) . iterate f
+        where f n = if even n then n `div` 2 else 3 * n + 1
