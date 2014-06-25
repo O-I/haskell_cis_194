3f206e3cf5b4d368e6dfd0fb5977f6e05bcebd8d
Rahul Horé <hore.rahul@gmail.com>
2014-06-24 21:40:57 -0400

    Adds foldTree function
    I get the gist of using `foldr` to convert a list into a binary tree (I used it to build the `MessageTree` in `LogAnalysis.hs`), but I had trouble implementing an `insert` function so that the tree always remains balanced. I looked at [Build balanced binary tree with foldr](http://stackoverflow.com/questions/16157203/build-balanced-binary-tree-with-foldr) on Stack Overflow for guidance. I'm not wild about all the `where` clauses, but it seems to work. I'll see if I can refactor it after a little more thought.

---
+++ hw4/folds.hs
@@ -0,0 +1,24 @@
+-- Exercise 2  Folding with trees
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+insert :: a -> Tree a -> Tree a
+insert item Leaf = Node 0 Leaf item Leaf
+insert item (Node i l m r)
+  | lh < rh   = Node i li m r
+  | lh > rh   = Node i l m ri
+  | otherwise = Node (h + 1) l m ri
+  where lh = height l
+        rh = height r
+        li = insert item l
+        ri = insert item r
+        h  = height ri
+
+height :: Tree a -> Integer
+height Leaf           = -1
+height (Node i l m r) = i
