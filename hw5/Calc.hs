171696cfc0163b8ac11ef179f4e960f357dd52a1
Rahul Horé <hore.rahul@gmail.com>
2014-07-08 14:04:02 -0400

    Adds eval function

---
+++ hw5/Calc.hs
@@ -0,0 +1,10 @@
+module Calc where
+
+import ExprT
+
+-- Exercise 1
+
+eval :: ExprT -> Integer
+eval (Lit x)       = x
+eval (Add (x) (y)) = (eval x) + (eval y)
+eval (Mul (x) (y)) = (eval x) * (eval y)
