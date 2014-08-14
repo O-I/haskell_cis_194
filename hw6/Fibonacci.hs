d8719d492e6a7089e5c4e3f67d659bd616a3bdc7
Rahul Horé <hore.rahul@gmail.com>
2014-08-14 18:19:18 -0400

    Defines a Stream data type

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -34,3 +34,14 @@ fibs'' = 0 : 1 : [ a+b | a <- fibs'' | b <- tail fibs'' ]
 
 fibs''' :: [Integer]
 fibs''' = 0 : scanl (+) 1 fibs'''
+
+-- Exercise 3
+
+data Stream a = Cons a (Stream a)
+  deriving (Eq)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
