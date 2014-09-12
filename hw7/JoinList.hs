30f2fa468e155195f319f540d6e74b3032e5d232
Rahul Horé <hore.rahul@gmail.com>
2014-09-11 20:14:43 -0400

    Adds tag function
    Gets the annotation at the root of a JoinList

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -1,6 +1,13 @@
 module JoinList where
 
-  data JoinList m a = Empty
-                    | Single m a
-                    | Append m (JoinList m a) (JoinList m a)
-    deriving (Eq, Show)
+import Data.Monoid
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty          = mempty
+tag (Single m _)   = m
+tag (Append m _ _) = m
