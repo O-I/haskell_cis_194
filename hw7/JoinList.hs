70c2345c91288306f6f030ba93a98687ed589428
Rahul Horé <hore.rahul@gmail.com>
2014-09-12 20:43:25 -0400

    Adds safe indexing and jlTolist for testing

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -1,11 +1,14 @@
 module JoinList where
 
 import Data.Monoid
+import Sized
 
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
   deriving (Eq, Show)
+
+-- Exercise 1
 
 tag :: Monoid m => JoinList m a -> m
 tag Empty          = mempty
@@ -14,3 +17,19 @@ tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 jl1 +++ jl2 = Append (tag jl1 <> tag jl2) jl1 jl2
+
+-- Exercise 2
+
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _         = Nothing
+_      !!? i | i < 0 = Nothing
+(x:xs) !!? 0         = Just x
+(x:xs) !!? i         = xs !!? (i - 1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty            = []
+jlToList (Single _ a)     = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ = _
