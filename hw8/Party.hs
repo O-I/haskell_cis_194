c356cd3258ecc5d68cecf067f06872578d58e2ef
Rahul Horé <hore.rahul@gmail.com>
2014-10-22 18:16:04 -0400

    Adds Monoid instance of GuestList

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -1,8 +1,13 @@
 module Party where
 
 import Employee
+import Data.Monoid
 
 -- Exercise 1
 
 glCons :: Employee -> GuestList -> GuestList
 glCons emp (GL empList fun) = GL (emp : empList) (fun + empFun emp)
+
+instance Monoid GuestList where
+  mempty                        = GL [] 0
+  mappend (GL l1 f1) (GL l2 f2) = GL (l1 ++ l2) (f1 + f2)
