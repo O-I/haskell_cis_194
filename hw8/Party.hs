2e85b3662f2c09a69c9d9edb7e91a2ffcffed33e
Rahul Horé <hore.rahul@gmail.com>
2014-10-23 19:04:19 -0400

    Adds moreFun function

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Party where
 
 import Employee
@@ -11,3 +13,6 @@ glCons emp (GL empList fun) = GL (emp : empList) (fun + empFun emp)
 instance Monoid GuestList where
   mempty                        = GL [] 0
   mappend (GL l1 f1) (GL l2 f2) = GL (l1 ++ l2) (f1 + f2)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun = max
